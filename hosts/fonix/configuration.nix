2df80c7509a731bf1dbf0989f0e2f104ef2c1628
Pedro Algarvio <pedro@algarvio.me>
2024-05-22 22:35:04 +0100

    Don't change the password that is set

--- hosts/fonix/configuration.nix
+++ hosts/fonix/configuration.nix
@@ -86,10 +86,6 @@
 
   users.users = {
     vampas = {
-      # You can set an initial password for your user.
-      # If you do, you can skip setting a root password by passing '--no-root-passwd' to nixos-install.
-      # Be sure to change it (using passwd) after rebooting!
-      initialPassword = "my-password";
       isNormalUser = true;
       openssh.authorizedKeys.keys = [
         # Add your SSH public key(s) here, if you plan on using SSH to connect

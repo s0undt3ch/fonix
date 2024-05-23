8bdde4d739b6bc0ce7e686b5d8ba9ccff0de0ea4
Pedro Algarvio <pedro@algarvio.me>
2024-05-23 08:39:35 +0100

    `vim` as the default editor

--- hosts/fonix/configuration.nix
+++ hosts/fonix/configuration.nix
@@ -148,6 +148,9 @@
   # Install firefox.
   programs.firefox.enable = true;
 
+  # Vim should be the default editor
+  programs.vim.defaultEditor = true;
+
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [

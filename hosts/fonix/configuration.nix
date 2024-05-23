9a6d7202dd49e8c3ef648bb3577985ac6d3b397c
Pedro Algarvio <pedro@algarvio.me>
2024-05-23 14:01:32 +0100

    Enable ssh

--- hosts/fonix/configuration.nix
+++ hosts/fonix/configuration.nix
@@ -89,6 +89,7 @@
       isNormalUser = true;
       openssh.authorizedKeys.keys = [
         # Add your SSH public key(s) here, if you plan on using SSH to connect
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICMmO8zkmuBXHRLg4OOWUiImpgqwEQqt5JGiKB09XO+q"
       ];
       # Be sure to add any other groups you need (such as networkmanager, audio, docker, etc)
       extraGroups = [

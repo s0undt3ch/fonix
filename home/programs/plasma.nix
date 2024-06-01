ac2fc181a726e7ca4c1e5a04570c0bde51adb9fe
Pedro Algarvio <pedro@algarvio.me>
2024-06-01 06:33:18 +0100

    Update plasma configs

--- home/programs/plasma.nix
+++ home/programs/plasma.nix
@@ -10,13 +10,21 @@
       lookAndFeel = "org.kde.breezedark.desktop";
       #cursorTheme = "Bibata-Modern-Ice";
       #iconTheme = "Papirus-Dark";
-      #wallpaper = "${pkgs.kdePackages.plasma-workspace-wallpapers}/share/wallpapers/Patak/contents/images/1080x1920.png";
+      wallpaper = "${pkgs.kdePackages.plasma-workspace-wallpapers}/share/wallpapers/Flow";
     };
 
-    hotkeys.commands."launch-terminal" = {
-      name = "Launch Terminal";
-      key = "Meta+T";
-      command = "kitty";
+    kwin = {
+      virtualDesktops = {
+        rows = 2;
+        number = 4;
+        names = ["Desktop 1" "Desktop 2" "Desktop 3" "Desktop 4"];
+      };
+    };
+
+    shortcuts = {
+      "firefox.desktop"."_launch" = "Meta+F";
+      "kitty.desktop"."_launch" = "Meta+T";
+      "org.kde.dolphin.desktop"."_launch" = "Meta+D";
     };
 
     panels = [
@@ -41,17 +49,18 @@
           # pin apps to the task-manager, which this example illustrates by
           # pinning dolphin and konsole to the task-manager by default.
           {
-            name = "org.kde.plasma.icontasks";
+            name = "org.kde.plasma.taskmanager";
             config = {
               General.launchers = [
-                "applications:org.kde.dolphin.desktop"
-                "applications:org.kde.konsole.desktop"
+                # "applications:org.kde.dolphin.desktop"
+                # "applications:org.kde.konsole.desktop"
               ];
             };
           }
           # If no configuration is needed, specifying only the name of the
           # widget will add them with the default configuration.
           "org.kde.plasma.marginsseparator"
+          "org.kde.plasma.pager"
           # If you need configuration for your widget, instead of specifying the
           # the keys and values directly using the config attribute as shown
           # above, plasma-manager also provides some higher-level interfaces for
@@ -61,36 +70,35 @@
           # first day of the week to sunday and another adding a systray with
           # some modifications in which entries to show.
           {
+            systemTray = {
+              icons = {
+                spacing = "small";
+                scaleToFit = false;
+              };
+              items = {
+                #    # We explicitly show bluetooth and battery
+                #    shown = [
+                #      "org.kde.plasma.battery"
+                #      "org.kde.plasma.bluetooth"
+                #    ];
+                hidden = [
+                  "kde.plasma.brightness"
+                ];
+                configs = {
+                  battery.showPercentage = true;
+                };
+              };
+            };
+          }
+          {
             digitalClock = {
               calendar.firstDayOfWeek = "monday";
               time.format = "24h";
             };
           }
-          #{
-          #  systemTray.items = {
-          #    # We explicitly show bluetooth and battery
-          #    shown = [
-          #      "org.kde.plasma.battery"
-          #      "org.kde.plasma.bluetooth"
-          #    ];
-          #    # And explicitly hide networkmanagement and volume
-          #    hidden = [
-          #      "org.kde.plasma.networkmanagement"
-          #      "org.kde.plasma.volume"
-          #    ];
-          #  };
-          #}
         ];
-        hiding = "autohide";
+        hiding = "none";
       }
-      # Global menu at the top
-      #{
-      #  location = "top";
-      #  height = 26;
-      #  widgets = [
-      #    "org.kde.plasma.appmenu"
-      #  ];
-      #}
     ];
 
     #
@@ -110,6 +118,59 @@
       #  # Provider will be added to [Greeter][Wallpaper][org.kde.potd][General].
       #  "Greeter/Wallpaper/org.kde.potd/General".Provider = "bing";
       #};
+      kxkbrc."Layout" = {
+        "DisplayNames" = ",";
+        "LayoutList" = "us,pt";
+        "Options" = "grp:shifts_toggle";
+        "ResetOldOptions" = true;
+        "Use" = true;
+        "VariantList" = ",";
+      };
+      kcminputrc = {
+        "Keyboard"."NumLock" = 0;
+      };
+
+      kwinrc = {
+        "NightColor"."Active".value = true;
+        "NightColor"."Mode".value = "Location";
+        "NightColor"."LatitudeAuto".value = 38.9;
+        "NightColor"."LongitudeFixed".value = -9.03;
+        "NightColor"."NightTemperature".value = 4400;
+      };
+
+      kglobalshortcutsrc = {
+        kwin = {
+          "Window One Screen to the Left" = "Ctrl+Alt+Left";
+          "Window One Screen to the Right" = "Ctrl+Alt+Right";
+        };
+      };
+
+      powerdevilrc = {
+        "AC/Display" = {
+          DimDisplayIdleTimeoutSec = 900;
+          TurnOffDisplayIdleTimeoutSec = 3600;
+        };
+        "AC/Keyboard" = {
+          KeyboardBrightness = 100;
+        };
+        "AC/Performance" = {
+          PowerProfile = "performance";
+        };
+        "AC/SuspendAndShutdown" = {
+          AutoSuspendAction = 0;
+          PowerButtonAction = 1;
+        };
+        "Battery/Performance" = {
+          PowerProfile = "balanced";
+        };
+        "LowBattery/Keyboard" = {
+          KeyboardBrightness = 15;
+          UseProfileSpecificKeyboardBrightness = true;
+        };
+        "LowBattery/Performance" = {
+          PowerProfile = "power-saver";
+        };
+      };
     };
   };
 }

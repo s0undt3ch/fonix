82ee39bd2e0091d2358ad9b856801e2f58483472
Pedro Algarvio <pedro@algarvio.me>
2024-06-03 18:17:12 +0100

    Git config updates

--- home/programs/git.nix
+++ home/programs/git.nix
@@ -4,6 +4,10 @@
     enable = true;
     userName = "Pedro Algarvio";
     userEmail = "pedro@algarvio.me";
+    signing = {
+      key = "84A298FF";
+      signByDefault = true;
+    };
     ignores = [
       ".config*/"
       ".project"
@@ -16,10 +20,27 @@
       # Vim Swap Files
       ".*.sw*"
     ];
+    aliases = {
+      l = "log --oneline --decorate";
+      ll = "log --stat --decorate --abbrev-commit --date=relative";
+      lll = "log -p --stat --decorate --abbrev-commit --date=relative";
+      sbranch = ''
+        for-each-ref --sort=committerdate refs/heads/ --format='%(HEAD) %(color:yellow)%(refname:short)%(color:reset) - %(color:red)%(objectname:short)%(color:reset) - %(contents:subject) - %(authorname) (%(color:green)%(committerdate:relative)%(color:reset))'
+      '';
+    };
     extraConfig = {
+      #core = {
+      #  pager = "cat";
+      #  diff = "less -FX";
+      #};
+      grep = {
+        linenumber = true;
+      };
       init = {
         defaultBranch = "main";
       };
     };
   };
+
+  programs.gitui.enable = true;
 }

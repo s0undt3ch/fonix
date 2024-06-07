9192c9cb70e955bca1865ada8139de7dcef9995b
Pedro Algarvio <pedro@algarvio.me>
2024-06-07 14:07:37 +0100

    Add VLC

--- home/users/vampas/fonix.nix
+++ home/users/vampas/fonix.nix
@@ -1,9 +1,15 @@
-{ ... }:
+{ pkgs, ... }:
 {
-  # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
-  home.stateVersion = "24.05";
   imports = [
     ./common.nix
+    ../../programs/vlc.nix
     ../../programs/user-scripts.nix
   ];
+
+  home = {
+    packages = with pkgs; [ ];
+
+    # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
+    stateVersion = "24.05";
+  };
 }
